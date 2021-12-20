27ce3f2d631da993a8ca339d8dbc34269bbaad6a
Igor Giussani <gius80@gmail.com>
2021-12-20 19:44:18 +0100

    add dns detail button link

--- client/my-sites/domains/domain-management/settings/index.tsx
+++ client/my-sites/domains/domain-management/settings/index.tsx
@@ -112,6 +112,7 @@ const Settings = ( {
 					isRequestingDomains={ isRequestingDomains }
 					selectedDomainName={ selectedDomainName }
 					selectedSite={ selectedSite }
+					currentRoute={ currentRoute }
 				/>
 			</Accordion>
 		);
